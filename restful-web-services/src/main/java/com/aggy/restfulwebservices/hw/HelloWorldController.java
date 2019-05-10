71b789c0ec2a79ffb0f42ef522df5bc6f6a9503d
Kamil Augustyn <augus_k@best.com.pl>
2019-05-10 09:38:16 +0200

    i18n

--- restful-web-services/src/main/java/com/aggy/restfulwebservices/hw/HelloWorldController.java
+++ restful-web-services/src/main/java/com/aggy/restfulwebservices/hw/HelloWorldController.java
@@ -1,9 +1,15 @@
 package com.aggy.restfulwebservices.hw;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.Locale;
 
 @RestController
 public class HelloWorldController {
+    @Autowired
+    private MessageSource messageSource;
 
     @RequestMapping(method = RequestMethod.GET, path = "/hello-world")
     public String getHelloWorld() {
@@ -19,4 +25,10 @@ public class HelloWorldController {
     public HelloWorldBean getHelloWorldBeanWithParam(@PathVariable String param) {
         return new HelloWorldBean(String.format("Hello World, %s", param));
     }
+
+    @RequestMapping(method = RequestMethod.GET, path = "/hello-world-in")
+    public String getHelloWorldIn(@RequestHeader(name = "Accept-Language", required = false) Locale locale) {
+        return messageSource.getMessage("good.morning.message",null, locale);
+    }
+
 }
